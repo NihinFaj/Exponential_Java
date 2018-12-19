a8661c62fc955b701ba359fbc3728c046ba6a7ab
Nihinlolamiwa Fajemilehin <nihinfaj@gmail.com>
2018-12-19 14:10:53 +0000

    Initial sharing of project

---
+++ Exponential.java
@@ -0,0 +1,27 @@
+/**
+ * This class calculates the exponential of a number
+ *
+ * @author (Nihinlolamiwa Fajemilehin)
+ * @version (19th of December 2018)
+ */
+public class Exponential
+{
+    /**
+     * Constructor for objects of class Exponential
+     */
+    public Exponential()
+    {
+    }
+
+    /**
+     * Method that calculates the exponential of a number
+     * @param a The number exponential is to be calculated upon
+     * @param n The exponential value
+     */
+    public void exp() 
+    {
+        // if() {
+            
+        // }
+    }
+}
